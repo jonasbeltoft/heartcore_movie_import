2a1d7af56f6e92960588cb1f940ac161c672bca3
jonasbeltoft <jonasb5@hotmail.dk>
2025-02-13 15:23:01 +0100

    base impl

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var baseUrl = "https://api.tvmaze.com/shows?page="
 
 func main() {
-    fmt.Println("Hello, World!")
+	page := 0
+
+	response, err := http.Get(baseUrl + strconv.Itoa(page))
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(response.ContentLength)
+}
+
+type TVMazeShow struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name,omitempty"`
+	Image string `json:"image,omitempty"`
+}
+
+type Image struct {
+	Medium   string `json:"medium,omitempty"`
+	Original string `json:"original,omitempty"`
 }
